2ccd593559a8a15bbdf474aedf0078d4700a4add
mpriyankdev <mpriyank.dev@gmail.com>
2020-01-21 21:27:53 +0530

    initial commit

---
+++ config-client/src/main/java/com/practise/configclient/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.practise.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+public class TestController {
+
+    @Value("${lucky-word}")
+    private String luckyWord;
+
+    @GetMapping("/lucky-word")
+    public ResponseEntity<String> luckyWord() {
+
+        String str = "Todays lucky word is : " + luckyWord;
+
+        return new ResponseEntity<>(luckyWord, HttpStatus.OK);
+
+    }
+}
